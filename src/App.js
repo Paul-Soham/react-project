b741a1d7d06b9d16099e2e1dc0094aac33762aff
Your Name <you@example.com>
2023-04-30 23:18:04 +0530

    Added a welcome massage for testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div><h1>Hello World</h1></div>
+        <div><h1>Hello World</h1>
+        <h2>Welcome to my react project</h2>
+        </div>
       </header>
     </div>
   );
